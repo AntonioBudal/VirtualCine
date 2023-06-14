fe97258e3d445ecc7dd61814f446a67a3857c0c7
Antônio Budal <antonio_b_lima@estudante.sesisenai.org.br>
2023-06-13 21:23:54 -0300

    Começando a Tela de Planos

--- configuracoes/configuracoes.js
+++ configuracoes/configuracoes.js
@@ -3,3 +3,8 @@ function mudarConfiguracao(config){
     $('#'+config).css("display", "flex");
     $('#'+config).siblings().css("display", "none");
 }
+
+function abrirTela(pasta,arquivo){
+    let str = '../'+pasta+'/'+arquivo;
+    location.href = str;
+}
